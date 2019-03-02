0f8e2591f667b57e30fa621e7148318d464b3abb
Alex <brandelt4@gmail.com>
2019-03-02 22:27:33 +0000

    120

--- comp0037/comp0037_explorer/src/comp0037_explorer/explorer_node.py
+++ comp0037/comp0037_explorer/src/comp0037_explorer/explorer_node.py
@@ -1,6 +1,12 @@
+import sys
+sys.path.append('/home/ros_user/catkin_ws/src/comp0037/comp0037_reactive_planner_controller/src/comp0037_reactive_planner_controller')
+
+
 import rospy
+from math import sqrt
 
 from explorer_node_base import ExplorerNodeBase
+import reactive_planner_controller
 
 # This class implements a super dumb explorer. It goes through the
 # current map and marks the first cell it sees as the one to go for
@@ -12,6 +18,10 @@ class ExplorerNode(ExplorerNodeBase):
 
         self.blackList = []
 
+    def calculateHeuristic(self, start, goal):
+        dX = start[0] - goal[0]
+        dY = start[1] - goal[1]
+        return sqrt(dX ** 2 + dY ** 2)
 
     def chooseNewDestination(self):
 
@@ -20,19 +30,27 @@ class ExplorerNode(ExplorerNodeBase):
 #         for coords in self.blackList:
 #             print str(coords)
 
+        costs = []
+        candidateGood = False
 
+
+        pose = self.controller.getCurrentPose()
+        start = (pose.x, pose.y)
+        startCellCoords = self.occupancyGrid.getCellCoordinatesFromWorldCoordinates(start)
         # Iterates through coordinates X and Y
 
-        for candidate in self.frontier:
-            print(candidate)
-            candidateGood = True
-            for k in range(0, len(self.blackList)):
-                if self.blackList[k] == candidate:
-                    candidateGood = False
-                    break
+        # NOW IMPLEMENT THE BEST WAY TO CHOOSE THE NEW DESTINATION
 
-            if candidateGood is True:
-                return True, candidate
+        for idx, candidate in enumerate(self.frontier):
+            candidateGood = True
+            costs.append(self.calculateHeuristic(candidate, startCellCoords))
+
+        nextOne = costs.index(min(costs))
+
+        if candidateGood is True:
+            return True, self.frontier[nextOne]
+
+
 
         return False, None
 
