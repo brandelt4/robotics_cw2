cd97e583ca7d837a9fdab434ea437177184fbdce
Alex <brandelt4@gmail.com>
2019-03-15 20:15:28 +0000

    asd

--- comp0037/comp0037_reactive_planner_controller/src/comp0037_reactive_planner_controller/reactive_planner_controller.py
+++ comp0037/comp0037_reactive_planner_controller/src/comp0037_reactive_planner_controller/reactive_planner_controller.py
@@ -14,6 +14,7 @@ class ReactivePlannerController(PlannerControllerBase):
 
         self.mapUpdateSubscriber = rospy.Subscriber('updated_map', MapUpdate, self.mapUpdateCallback)
         self.gridUpdateLock =  threading.Condition()
+        self.waypoints_counter = 0
 
     def mapUpdateCallback(self, mapUpdateMessage):
 
