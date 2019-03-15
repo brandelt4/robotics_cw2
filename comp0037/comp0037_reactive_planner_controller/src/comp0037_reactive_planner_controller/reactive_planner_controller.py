8b540ca04c243dfb66f2f789c1d2bef80e3623c1
Alex <brandelt4@gmail.com>
2019-03-15 16:39:58 +0000

    sd

--- comp0037/comp0037_reactive_planner_controller/src/comp0037_reactive_planner_controller/reactive_planner_controller.py
+++ comp0037/comp0037_reactive_planner_controller/src/comp0037_reactive_planner_controller/reactive_planner_controller.py
@@ -53,36 +53,36 @@ class ReactivePlannerController(PlannerControllerBase):
         for waypoint in self.currentPlannedPath.waypoints:
             # If the cell is occuppied, find a new path
             if self.occupancyGrid.getCell(waypoint.coords[0], waypoint.coords[1]) == 1.0:
-                # pose = self.controller.getCurrentPose()
-                # start = (pose.x, pose.y)
-                # currentCell = self.occupancyGrid.getCellCoordinatesFromWorldCoordinates(start)
-                # print('-'*40)
-                # print('CURRENT CELL POSITION: {}'.format(currentCell))
-                # print('WAYPOINT OCUPPIED: {}'.format(waypoint.coords))
+                pose = self.controller.getCurrentPose()
+                start = (pose.x, pose.y)
+                currentCell = self.occupancyGrid.getCellCoordinatesFromWorldCoordinates(start)
+                print('-'*40)
+                print('CURRENT CELL POSITION: {}'.format(currentCell))
+                print('WAYPOINT OCUPPIED: {}'.format(waypoint.coords))
 
 
                 # Calculate new path in the background
                 # Create a new planner object
-                # goalCellCoords = self.currentPlannedPath.waypoints[-1].coords
-                # self.planner_new = DijkstraPlanner('Dijkstra', self.occupancyGrid)
-                # self.planner_new.showGraphics = False
-                # self.planner_new.search(currentCell, goalCellCoords)
-                # self.newPlannedPath = self.planner_new.extractPathToGoal()
+                goalCellCoords = self.currentPlannedPath.waypoints[-1].coords
+                self.planner_new = DijkstraPlanner('Dijkstra', self.occupancyGrid)
+                self.planner_new.showGraphics = False
+                self.planner_new.search(currentCell, goalCellCoords)
+                self.newPlannedPath = self.planner_new.extractPathToGoal()
 
                 # If the new travel cost is 50% more, don't keep going
-                # if abs(int(self.newPlannedPath.travelCost) - int(self.currentPlannedPath.travelCost))/int(self.currentPlannedPath.travelCost) < 0.2:
-                # while (reached == False):
-                #     pose = self.controller.getCurrentPose()
-                #     start = (pose.x, pose.y)
-                #     currentCell = self.occupancyGrid.getCellCoordinatesFromWorldCoordinates(start)
-                #     if (abs(currentCell[0] - waypoint.coords[0]) < 4) and (abs(currentCell[1] - waypoint.coords[1]) < 4):
-                #         print('*****************'*20)
-                #         reached = True
+                if abs(int(self.newPlannedPath.travelCost) - int(self.currentPlannedPath.travelCost))/int(self.currentPlannedPath.travelCost) < 0.2:
+                    while (reached == False):
+                        pose = self.controller.getCurrentPose()
+                        start = (pose.x, pose.y)
+                        currentCell = self.occupancyGrid.getCellCoordinatesFromWorldCoordinates(start)
+                        if (abs(currentCell[0] - waypoint.coords[0]) < 4) and (abs(currentCell[1] - waypoint.coords[1]) < 4):
+                            print('*****************'*20)
+                            reached = True
 
-                self.controller.stopDrivingToCurrentGoal()
-                # break
-                # else:
-                #     self.controller.stopDrivingToCurrentGoal()
+                    self.controller.stopDrivingToCurrentGoal()
+                    break
+                else:
+                    self.controller.stopDrivingToCurrentGoal()
 
 
             else:
