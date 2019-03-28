d3d52ffbbf96786a57b2009750cc66223e98a707
Alex <brandelt4@gmail.com>
2019-03-28 18:54:54 +0000

    entropy + closest

--- comp0037/comp0037_mapper/src/comp0037_mapper/mapper_node.py
+++ comp0037/comp0037_mapper/src/comp0037_mapper/mapper_node.py
@@ -347,7 +347,7 @@ class MapperNode(object):
                 if self.occupancyGrid.getCell(x, y) == 0.5:
                     unknowns += 1
 
-        self.entropy = (unknowns * math.log(2))*math.log(unknowns * math.log(2))
+        self.entropy = (unknowns * math.log(2)) # *math.log(unknowns * math.log(2))
 
         with open('/home/ros_user/catkin_ws/src/comp0037/comp0037_explorer/src/comp0037_explorer/entropy.txt','a+') as file:
             file.write(str(self.entropy) + ',')
